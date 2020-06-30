ce85f3429539ab2aa4bfe8fea8d165c5e703f9bd
Kirk Roerig <kirk.roerig@advanced-space.com>
2020-06-30 06:25:59 -0600

    Working on downsampling

--- src/main.c
+++ src/main.c
@@ -1,6 +1,41 @@
 #include "vidi.h"
 
+typedef union {
+	struct {
+		uint8_t r, g, b;
+	};
+	uint8_t v[3];
+} rgb_t;
+typedef struct { 
+	uint8_t* frame,
+	size_t width, height;
+	size_t bytes_pixel;
+} frame_t; 
+
+
+void downsample(frame_t from, frame_t to)
+{
+	if (from.width < to.width) { return; }
+	if (from.height < to.height) { return; }
+
+	int dw = from.width / to.width;
+	int dh = from.height / to.height;
+
+	for 
+}
+
+
 int main (int argc, const char* argv[])
 {
+	vidi_cfg_t cam = {
+		.width = 640,
+		.height = 480,
+		.frames_per_sec = 60,
+		.path = argv[1]
+	};
+
+	assert(0 == vidi_config(&cam));
+	
+
 	return 0;
 }
